8ce86d5f01d86617d878d35eb12622f52b0c052f
Zachary Artman <zacharyartman@icloud.com>
2023-12-22 23:28:58 -0500

    revised html to use javascript by getting element as opposed to using onclick, keeping html more separate from js.

--- script.js
+++ script.js
@@ -4,3 +4,56 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView();
+}
+function scrollToExperience() {
+    document.getElementById('experience').scrollIntoView();
+}
+function scrollToProjects() {
+    document.getElementById('projects').scrollIntoView();
+}
+function scrollToContact() {
+    document.getElementById('contact').scrollIntoView();
+}
+
+document.getElementById('desktop-nav-about').onclick = function() {
+    scrollToAbout();
+}
+
+document.getElementById('desktop-nav-experience').onclick = function() {
+    scrollToExperience();
+}
+
+document.getElementById('desktop-nav-projects').onclick = function() {
+    scrollToProjects();
+}
+
+document.getElementById('desktop-nav-contact').onclick = function() {
+    scrollToContact();
+}
+
+document.getElementById('hamburger-icon').onclick = function() {
+    toggleMenu();
+}
+
+document.getElementById('hamburger-nav-about').onclick = function() {
+    toggleMenu();
+    scrollToAbout();
+}
+
+document.getElementById('hamburger-nav-experience').onclick = function() {
+    toggleMenu();
+    scrollToExperience();
+}
+
+document.getElementById('hamburger-nav-projects').onclick = function() {
+    toggleMenu();
+    scrollToProjects();
+}
+
+document.getElementById('hamburger-nav-contact').onclick = function() {
+    toggleMenu();
+    scrollToContact();
+}
